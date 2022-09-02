220980bcf052bb2bf9991976ee11bd6a8d1fc090
chigo01 <baruchfavour01@gmail.com>
2022-09-02 16:56:23 +0100

    'app'

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter2_app/model/transaction.dart';
 import 'package:flutter2_app/widgets/chart.dart';
 import 'package:flutter2_app/widgets/new_transaction.dart';
 import 'package:flutter2_app/widgets/transcation_list.dart';
 import 'package:flutter2_app/widgets/user_transaction.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  //disabling of landscape
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations(
+  //   [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  // );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -88,6 +96,8 @@ class _MyHomePageState extends State<MyHomePage> {
     //   date: DateTime.now(),
     // )
   ];
+  bool _showChart = false;
+
   List<Transaction> get _recentTransactions {
     return _transactions.where((transaction) {
       return transaction.date!.isAfter(
@@ -98,7 +108,8 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  void _addNewTransaction(String txTitle, double txAmount, DateTime chosenDate) {
+  void _addNewTransaction(
+      String txTitle, double txAmount, DateTime chosenDate) {
     final newTx = Transaction(
         amount: txAmount,
         title: txTitle,
@@ -132,28 +143,54 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: const Text('Personal Expenses'),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: () {
+            _startAddNewTransaction(context);
+          },
+        )
+      ],
+    );
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Personal Expenses'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: () {
-                _startAddNewTransaction(context);
-              },
-            )
-          ],
-        ),
+        appBar: appBar,
         body: SingleChildScrollView(
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Chart(_recentTransactions),
-              TransactionList(
-                transactions: _transactions,
-                deleteTx:  _deleteTransaction
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+            const Text('Show Chart'),
+              Switch(value: _showChart, onChanged: (bool value) {
+                setState(() {
+                  _showChart = value;
+                });
+              },),
+            ],),
+             _showChart ? Container(
+                height: (MediaQuery.of(context).size.height -
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.39, //30%
+                child: Chart(_recentTransactions),
+
+              ) :
+
+              Container(
+                //added the calculation to prevent scrollable
+                height: (MediaQuery.of(context).size.height -
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.7, //70
+                child: TransactionList(
+                    transactions: _transactions, deleteTx: _deleteTransaction),
               )
+
             ],
           ),
         ),
--- lib/widgets/transcation_list.dart
+++ lib/widgets/transcation_list.dart
@@ -3,7 +3,6 @@ import 'package:intl/intl.dart';
 
 import '../model/transaction.dart';
 
-
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final Function deleteTx;
@@ -12,61 +11,63 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 450,
-      child: transactions.isEmpty
-          ? Column(
-        children: <Widget>[
-          Text(
-            'No transactions added yet!',
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-              height: 200,
-              child: Image.asset(
-                'assets/images/waiting.png',
-                fit: BoxFit.cover,
-              )),
-        ],
-      )
-          : ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            elevation: 5,
-            margin: const EdgeInsets.symmetric(
-              vertical: 8,
-              horizontal: 5,
-            ),
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                child: Padding(
-                  padding: const EdgeInsets.all(6),
-                  child: FittedBox(
-                    child: Text('\$${transactions[index].amount}'),
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              double height = constraints.maxHeight;
+              return Column(
+                children: <Widget>[
+                  Text(
+                    'No transactions added yet!',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                      height: height * 0.6,
+                      child: Image.asset(
+                        'assets/images/waiting.png',
+                        fit: BoxFit.cover,
+                      )),
+                ],
+              );
+            },
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text('\$${transactions[index].amount}'),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    transactions[index].title!,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date!),
+                  ),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => deleteTx(transactions[index].id),
                   ),
                 ),
-              ),
-              title: Text(
-                transactions[index].title!,
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              subtitle: Text(
-                DateFormat.yMMMd().format(transactions[index].date!),
-              ),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete),
-                color: Theme.of(context).errorColor,
-                onPressed: () => deleteTx(transactions[index].id),
-              ),
-            ),
+              );
+            },
+            itemCount: transactions.length,
           );
-        },
-        itemCount: transactions.length,
-      ),
-    );
   }
 }
